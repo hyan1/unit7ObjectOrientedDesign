bd647f2aa585cb78ee9abd5cce72320c0c433093
hyan1 <hyan@NN123-HA-20.HS.sd203.org>
2016-02-11 14:12:58 -0600

    class

---
+++ Practice/TrangleGUIViewer.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import javax.swing.*;
+
+
+public class TrangleGUIViewer
+{    
+    public static void main(String[] args)
+    {
+        JFrame frame = new TriangleFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
---
+++ Practice/TriangleGUIComponent.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class TriangleGUIComponent
+{
+    private static final int a = 100;
+    private static final int b = 100;
+    private static final int c = 100;
+    
+    private Triangle trangle;
+
+    /**
+     * Default constructor for objects of class TrangleGUIComponent
+     */
+    public TriangleGUIComponent()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x+y;
+    }
+
+}
